6d0af5feb3c58aa44360675a8fdf57db9712b6c0
rahulshamkuwar <rahulshamkuwar20@gmail.com>
2023-02-02 15:54:29 -0700

    Finished implementing jwt for login, will implement register and then jwt for that

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,23 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
 import { PrismaService } from "./prisma.service";
 import { UsersModule } from "./users/users.module";
 import { UsersService } from "./users/users.service";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService, UsersService, PrismaService],
+  providers: [
+    AppService,
+    UsersService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Request, Post, UseGuards } from "@nestjs/common";
+import { Get } from "@nestjs/common/decorators";
+import { Public } from "src/utils/constants";
 import { AuthService } from "./auth.service";
+import { JwtAuthGuard } from "./jwt-auth.guard";
 import { LocalAuthGuard } from "./local-auth.guard";
 
 @Controller("auth")
@@ -8,13 +11,21 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post("/login")
+  @Public()
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
   @UseGuards(LocalAuthGuard)
   @Post("/register")
+  @Public()
   async register() {
     return null;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get("profile")
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
